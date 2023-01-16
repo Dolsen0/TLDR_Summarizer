192899290da9a4a343dd3df9b8131a71609188eb
dolsen0 <derek.olsen@gmail.com>
2023-01-15 21:30:46 -0500

    increased text output size and cleaned code

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from api_key import apiKey
 openai.api_key = apiKey
 
 message = input("\nwhat would you like to summarize?\n")
-
 response = openai.Completion.create(
     model= "text-davinci-002",
     prompt = f"summarize the following message, it's too long: {message}",
     temperature = .8,
-    max_tokens = 60,
+    max_tokens = 80,
     top_p = 1
 )
 
-print(response["choices"][0]["text"])
+tldr = response["choices"][0]["text"]
+
+print(tldr)
