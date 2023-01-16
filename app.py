5b27313a89b42c9f656e266c4725efca82b20148
dolsen0 <derek.olsen@gmail.com>
2023-01-15 21:26:07 -0500

    created TLDR short summary program to abbreviate text

---
+++ app.py
@@ -0,0 +1,16 @@
+import openai
+from api_key import apiKey
+
+openai.api_key = apiKey
+
+message = input("\nwhat would you like to summarize?\n")
+
+response = openai.Completion.create(
+    model= "text-davinci-002",
+    prompt = f"summarize the following message, it's too long: {message}",
+    temperature = .8,
+    max_tokens = 60,
+    top_p = 1
+)
+
+print(response["choices"][0]["text"])
